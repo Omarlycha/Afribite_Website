e15cf24d84ef101332450465f69298fc8c930b7e
Mag <166319982+magali-la@users.noreply.github.com>
2024-10-23 05:34:32 +0200

    fixed alignment for tile, updated wording and styling, firestore data added

--- src/components/OrderPopup.jsx
+++ src/components/OrderPopup.jsx
@@ -8,18 +8,19 @@ const OrderPopup = ({customerName, orderName, orderNumber, orderQuantity, orderP
         <div className="flex flex-col justify-center items-center bg-notif border-2 border-terra-cotta w-[281px] text-n-n1 font-light gap-y-5 py-[21px] px-[23px] rounded-lg leading-5">
             <div className="text-sm text-n-n1">
                 {orderStatus === "new" && (
-                    <div>
-                    <h2 className='text-xl font-normal text-p-button'>Order Details: #{orderNumber}</h2>
-                    <p>New order {orderNumber} from {customerName}!</p><br/>
-                    <p>Dish: {orderName}</p>
-                    <p>Quantity: {orderQuantity}x</p>
-                    <p>Total: £{orderPrice}</p><br/>
-                    <p>Please Start Preparing the Order.</p><br/>
-                    <p>You can view More Details or Update the Order Status.</p>
-                    <div>
+                    <div className='font-normal'>
+                        <h2 className='text-xl tfont-normal text-p-button'>Order Details: #{orderNumber}</h2>
+                        <p className='font-medium mt-2 text-n-n1'>New order #{orderNumber} from {customerName}!</p>
+                        <p className="font-medium mt-4 text-n-n2">Order Summary:</p>
+                        <p className='text-n-n2'>Dish: {orderName}</p>
+                        <p className='text-n-n2'>Quantity: {orderQuantity}x</p>
+                        <p className='text-n-n2'>Total: £{orderPrice}</p><br/>
+                        <p className='text-n-n2'>Please Start Preparing the Order.</p><br/>
+                        <p className='text-n-n2'>Update Order Status once it's Ready for Pickup.</p>
+                    <div className='mt-4 flex justify-center'>
                         <Button 
-                            text="Finished Cooking"
-                            className="bg-accent text-notif text-lg rounded-lg"
+                            text="Ready for Pickup"
+                            className="bg-accent text-notif rounded-lg font-medium"
                             onClick={() => updateOrderStatus("ongoing")}
                         />
                     </div>
@@ -29,33 +30,40 @@ const OrderPopup = ({customerName, orderName, orderNumber, orderQuantity, orderP
 
                 {orderStatus === "ongoing" && (
                     <div>
-                    <p>Order #324 for {customerName} is finished and ready for delivery or pickup.</p><br/>
-                    <p className="font-normal">Order Details:</p>
-                    <p>Dish: {orderName}</p>
-                    <p>Quantity: {orderQuantity}x</p>
-                    <p>Total: £{orderPrice}</p><br/>
-                    <p>Take the next step to complete the order.</p><br/>
-                    <Button
-                      text="Ready for Pickup"
-                      className="bg-accent text-notif text-lg p-4 rounded-lg"
-                      onClick={() => updateOrderStatus("pickup")}
-
-                    /> 
+                        <h2 className='text-xl font-normal text-p-button'>Order Details: #{orderNumber}</h2>
+                        <p className='font-medium mt-2 text-n-n1'>Order #{orderNumber} is being delivered to {customerName}.</p>
+                        <p className="font-medium mt-4 text-n-n2">Order Summary:</p>
+                        <p className='text-n-n2'>Dish: {orderName}</p>
+                        <p className='text-n-n2'>Quantity: {orderQuantity}x</p>
+                        <p className='text-n-n2'>Total: £{orderPrice}</p><br/>
+                        <p className='text-n-n2'>Take the next step to complete the order.</p>
+                        <div className='mt-4 flex justify-center'>
+                            <Button
+                            text="Complete Delivery"
+                            className="bg-accent text-notif text-lg p-4 rounded-lg font-medium"
+                            onClick={() => updateOrderStatus("delivered")}
+                            />   
+                        </div>
+                        
                     </div>             
                 )}
                 {orderStatus === "delivered" && (
                     <div>
-                    <p>The delivery for Order #324 has been completed! {customerName} has received their meal, and your service was outstanding.</p><br />
-                    <p className="font-normal">Order Details:</p>
-                    <p>Dish: {orderName}</p>
-                    <p>Quantity: {orderQuantity}x</p>
-                    <p>Total: £{orderPrice}</p>
-                    <p>Delivery Time: {deliveryTime}</p><br/>
-                    <p>Thank you for your commitment to great service!</p><br />
-                    <Button
-                      text="View Order History"
-                      className="bg-accent text-notif text-lg p-4 rounded-lg"
-                    />
+                        <h2 className='text-xl font-normal text-p-button'>Order Details: #{orderNumber}</h2>
+                        <p className='font-medium mt-2 text-n-n1'>Order #{orderNumber} was delivered to {customerName}, and your service was outstanding!</p>
+                        <p className="font-medium mt-4 text-n-n2">Order Summary:</p>
+                        <p className='text-n-n2'>Dish: {orderName}</p>
+                        <p className='text-n-n2'>Quantity: {orderQuantity}x</p>
+                        <p className='text-n-n2'>Total: £{orderPrice}</p>
+                        <p className='text-n-n2'>Delivery Time: {deliveryTime}</p><br/>
+                        <p className='text-n-n2'>Thank you for your commitment to great service!</p>
+                        <div className='mt-4 flex justify-center'>
+                            <Button
+                            text="View Order History"
+                            className="bg-accent text-notif text-lg p-4 rounded-lg font-medium"
+                            />
+                        </div>
+                        
                     </div>
                 )}
             </div>
